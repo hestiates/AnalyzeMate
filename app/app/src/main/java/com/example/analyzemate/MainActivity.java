037551c2f24fdd3a09c8b074fe98d0f7aa5a4462
hestiates <kirillovm712@gmail.com>
2024-03-19 19:45:09 +0300

    Added Bottom Navigation Bar

--- app/app/src/main/java/com/example/analyzemate/MainActivity.java
+++ app/app/src/main/java/com/example/analyzemate/MainActivity.java
@@ -10,20 +10,59 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        /*
+        * Настройка навигационной панели
+        * Задание начального экрана, добавление путей перехода
+        */
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int item_id = item.getItemId();
+            if (item_id == R.id.bottom_home){
+                return true;
+            } else if (item_id == R.id.bottom_search){
+                startActivity(new Intent(getApplicationContext(), SearchActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            } else if (item_id == R.id.bottom_stock){
+                startActivity(new Intent(getApplicationContext(), StockActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            } else if (item_id == R.id.bottom_notification){
+                startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            } else if (item_id == R.id.bottom_account){
+                startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            }
+            return false;
+        });
+        // -----------------------
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
     public void goToGraphActivity(View view) {
         Intent intent = new Intent(this, GraphActivity.class);
         startActivity(intent);
