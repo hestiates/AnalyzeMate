02a9bbbadba646506f9c379edcaff0bc2a420350
hestiates <kirillovm712@gmail.com>
2024-03-19 19:47:32 +0300

    import optimize

--- app/app/src/main/java/com/example/analyzemate/StockActivity.java
+++ app/app/src/main/java/com/example/analyzemate/StockActivity.java
@@ -5,9 +5,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
