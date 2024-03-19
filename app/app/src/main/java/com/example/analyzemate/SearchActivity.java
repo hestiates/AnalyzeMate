b8f92cd12a9b00cb8fcf182a3ea57ae0c806a29a
hestiates <kirillovm712@gmail.com>
2024-03-19 19:44:34 +0300

    Added activity

---
+++ app/app/src/main/java/com/example/analyzemate/SearchActivity.java
@@ -0,0 +1,57 @@
+package com.example.analyzemate;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search);
+        /*
+         * Настройка навигационной панели
+         * Задание начального экрана, добавление путей перехода
+         */
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_search);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int item_id = item.getItemId();
+            if (item_id == R.id.bottom_home){
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            } else if (item_id == R.id.bottom_search){
+                return true;
+            } else if (item_id == R.id.bottom_stock){
+                startActivity(new Intent(getApplicationContext(), StockActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            } else if (item_id == R.id.bottom_notification){
+                startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            } else if (item_id == R.id.bottom_account){
+                startActivity(new Intent(getApplicationContext(), AccountActivity.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left);
+                finish();
+                return true;
+            }
+            return false;
+        });
+        // -----------------------
+    }
+}
