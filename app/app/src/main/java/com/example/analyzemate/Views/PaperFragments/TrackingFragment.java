11c3096b4687eb0ba423d44c6cc81186d4480066
hestiates <kirillovm712@gmail.com>
2024-04-06 12:06:19 +0300

    minor fix

--- app/app/src/main/java/com/example/analyzemate/Views/PaperFragments/TrackingFragment.java
+++ app/app/src/main/java/com/example/analyzemate/Views/PaperFragments/TrackingFragment.java
@@ -1,12 +1,11 @@
 package com.example.analyzemate.Views.PaperFragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.analyzemate.R;
 
@@ -16,47 +15,6 @@ import com.example.analyzemate.R;
  * create an instance of this fragment.
  */
 public class TrackingFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TrackingFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TrackingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TrackingFragment newInstance(String param1, String param2) {
-        TrackingFragment fragment = new TrackingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
