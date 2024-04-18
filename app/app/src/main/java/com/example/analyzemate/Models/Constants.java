cca70c2c24277e052054e44c01d9abeadb882de9
levchig737 <levchekarev3@gmail.com>
2024-04-18 17:09:07 +0300

    serverUrl вынесен в отдельный класс Constants

---
+++ app/app/src/main/java/com/example/analyzemate/Models/Constants.java
@@ -0,0 +1,9 @@
+package com.example.analyzemate.Models;
+
+public class Constants {
+    public static final String SERVER_URL = "http://192.168.3.234:8000/";
+
+    // Лев
+    // моб инет: http://192.168.74.5:8000/
+    // вай фай общага: http://192.168.3.234:8000/
+}
