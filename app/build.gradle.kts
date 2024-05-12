c0976af6769fad2657b46d5b3592e510ad464805
levchig737 <levchekarev3@gmail.com>
2024-05-12 23:13:01 +0300

    Исправлены зависимости firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 buildscript {
     dependencies {
-        classpath(libs.google.services)
+//        classpath(libs.google.services)
+        classpath("com.google.gms:google-services:4.4.1")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
